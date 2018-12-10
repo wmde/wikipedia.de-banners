5d3c0973254939a5d9aae1c46a49deb41ad3698e
Tim Eulitz <tim.eulitz@wikimedia.de>
2018-12-10 17:25:35 +0100

    Update banner campaign and add stats file

---
+++ stats.js
@@ -0,0 +1,12 @@
+GlobalBannerSettings = {
+	'goalDonationSum': 8100000,
+	'donations-date-base': '2018-12-10',
+	'donations-collected-base': 5077546,
+	'donators-base': 223773,
+	'appr-donations-per-minute': 104,
+	'appr-donators-per-minute': 4.5,
+	'impressions-per-day-in-million': 7,
+	'campaign-start-date': '2018-11-13',
+	'campaign-end-date': '2018-12-31',
+	'banner-close-track-ratio': 1
+};
