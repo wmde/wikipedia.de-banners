6710947158a348f5f0c1ba00aff9b16fe100ecb2
Gabriel Birke <gabriel.birke@gmail.com>
2018-12-17 10:38:26 +0100

    Adapted numbers for 2018-12-16

--- stats.js
+++ stats.js
@@ -1,8 +1,8 @@
 GlobalBannerSettings = {
 	'goalDonationSum': 8100000,
-	'donations-date-base': '2018-12-14',
-	'donations-collected-base': 5871342,
-	'donators-base': 257004,
+	'donations-date-base': '2018-12-16',
+	'donations-collected-base': 6244219,
+	'donators-base': 275392,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 4.5,
 	'impressions-per-day-in-million': 7,
