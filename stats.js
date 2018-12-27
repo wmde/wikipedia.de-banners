d9786d84cc21d45f43c20d884f3e73c1edb772b0
KaiNissen <kai.nissen@wikimedia.de>
2018-12-27 11:40:42 +0100

    update projection values

--- stats.js
+++ stats.js
@@ -1,11 +1,11 @@
 GlobalBannerSettings = {
 	'goalDonationSum': 8100000,
-	'donations-date-base': '2018-12-26',
-	'donations-collected-base': 7646988,
-	'donators-base': 333093,
+	'donations-date-base': '2018-12-27',
+	'donations-collected-base': 7724433,
+	'donators-base': 337240,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 4.5,
-	'impressions-per-day-in-million': 7,
+	'impressions-per-day-in-million': 6,
 	'campaign-start-date': '2018-11-13',
 	'campaign-end-date': '2018-12-31',
 	'banner-close-track-ratio': 1
