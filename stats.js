18f71ae431eecafea28277e09f198c758c702894
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-10-14 16:24:48 +0200

    Add Svelte-based and remove obsolete banners

--- stats.js
+++ stats.js
@@ -1,12 +1,12 @@
 GlobalBannerSettings = {
 	'goalDonationSum': 8100000,
-	'donations-date-base': '2018-12-27',
-	'donations-collected-base': 7724433,
+	'donations-date-base': '2019-11-11',
+	'donations-collected-base': 100000,
 	'donators-base': 337240,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 4.5,
 	'impressions-per-day-in-million': 6,
-	'campaign-start-date': '2018-11-13',
-	'campaign-end-date': '2018-12-31',
+	'campaign-start-date': '2019-11-11',
+	'campaign-end-date': '2019-12-31',
 	'banner-close-track-ratio': 1
 };
