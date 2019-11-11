e01c6f3134ae1a2c8c50f4cb138dfbc13f8373d2
KaiNissen <kai.nissen@wikimedia.de>
2019-11-11 11:03:16 +0100

    update values for campaign start

--- stats.js
+++ stats.js
@@ -1,11 +1,11 @@
 GlobalBannerSettings = {
-	'goalDonationSum': 8100000,
+	'goalDonationSum': 8700000,
 	'donations-date-base': '2019-11-11',
-	'donations-collected-base': 100000,
-	'donators-base': 337240,
+	'donations-collected-base': 50000,
+	'donators-base': 0,
 	'appr-donations-per-minute': 104,
-	'appr-donators-per-minute': 4.5,
-	'impressions-per-day-in-million': 6,
+	'appr-donators-per-minute': 3.8,
+	'impressions-per-day-in-million': 7,
 	'campaign-start-date': '2019-11-11',
 	'campaign-end-date': '2019-12-31',
 	'banner-close-track-ratio': 1
