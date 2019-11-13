a887feec3dc53afa5e53dcaf1c5bd926a7143842
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-11-13 09:22:26 +0100

    Update global banner values

--- stats.js
+++ stats.js
@@ -1,11 +1,11 @@
 GlobalBannerSettings = {
 	'goalDonationSum': 8700000,
-	'donations-date-base': '2019-11-11',
-	'donations-collected-base': 50000,
-	'donators-base': 0,
+	'donations-date-base': '2019-11-12',
+	'donations-collected-base': 400000,
+	'donators-base': 17363,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 3.8,
-	'impressions-per-day-in-million': 7,
+	'impressions-per-day-in-million': 8,
 	'campaign-start-date': '2019-11-11',
 	'campaign-end-date': '2019-12-31',
 	'banner-close-track-ratio': 1
