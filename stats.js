c8347d2fe54dc3ec82c22c0320900bdf1717954b
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-11-13 10:57:06 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,11 +1,11 @@
 GlobalBannerSettings = {
 	'goalDonationSum': 8700000,
-	'donations-date-base': '2019-11-12',
-	'donations-collected-base': 400000,
-	'donators-base': 17363,
+	'donations-date-base': '2019-11-13',
+	'donations-collected-base': 750000,
+	'donators-base': 36477,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 3.8,
-	'impressions-per-day-in-million': 8,
+	'impressions-per-day-in-million': 9,
 	'campaign-start-date': '2019-11-11',
 	'campaign-end-date': '2019-12-31',
 	'banner-close-track-ratio': 1
