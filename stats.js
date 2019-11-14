d1c01197a814e09c5e26ce40d41ceefa50ac930a
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-11-14 10:22:09 +0100

    Update camapign parameters
    Moved Goal sum down to avoid accidentally chaning it.

--- stats.js
+++ stats.js
@@ -1,12 +1,12 @@
 GlobalBannerSettings = {
-	'goalDonationSum': 8700000,
-	'donations-date-base': '2019-11-13',
-	'donations-collected-base': 750000,
-	'donators-base': 36477,
+	'donations-date-base': '2019-11-14',
+	'donations-collected-base': 1056000,
+	'donators-base': 52622,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 3.8,
 	'impressions-per-day-in-million': 9,
 	'campaign-start-date': '2019-11-11',
 	'campaign-end-date': '2019-12-31',
-	'banner-close-track-ratio': 1
+	'banner-close-track-ratio': 1,
+	'goalDonationSum': 8700000
 };
