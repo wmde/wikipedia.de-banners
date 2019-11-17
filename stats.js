ab06e4456e1077dcca9f095615708d0d386aa64c
KaiNissen <kai.nissen@wikimedia.de>
2019-11-17 13:29:37 +0100

    daily update

--- stats.js
+++ stats.js
@@ -1,7 +1,7 @@
 GlobalBannerSettings = {
-	'donations-date-base': '2019-11-16',
-	'donations-collected-base': 1542964,
-	'donators-base': 78673,
+	'donations-date-base': '2019-11-17',
+	'donations-collected-base': 1687558,
+	'donators-base': 86704,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 3.8,
 	'impressions-per-day-in-million': 8,
