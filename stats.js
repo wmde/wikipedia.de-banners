26668ecd9a81cf95f3cafa7c42a765127acdb64c
Co <corinna.hillebrand@wikimedia.de>
2019-12-16 10:36:06 +0100

    update stat parameters to current day

--- stats.js
+++ stats.js
@@ -1,7 +1,7 @@
 GlobalBannerSettings = {
-	'donations-date-base': '2019-12-15',
-	'donations-collected-base': 7136175,
-	'donators-base': 330428,
+	'donations-date-base': '2019-12-16',
+	'donations-collected-base': 7283713,
+	'donators-base': 337263,
 	'appr-donations-per-minute': 104,
 	'appr-donators-per-minute': 3.8,
 	'impressions-per-day-in-million': 6,
