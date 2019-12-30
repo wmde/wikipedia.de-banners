0ba07fe04e98636314b7175a129faeb2f8c82759
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-12-30 17:12:48 +0100

    Update global banner settings
    Update number of donors
    Store number of members

--- stats.js
+++ stats.js
@@ -1,12 +1,13 @@
 GlobalBannerSettings = {
 	'donations-date-base': '2019-12-20',
 	'donations-collected-base': 8001705,
-	'donators-base': 363998,
+	'donators-base': 403966,
 	'appr-donations-per-minute': 97,
 	'appr-donators-per-minute': 3.8,
 	'impressions-per-day-in-million': 6,
 	'campaign-start-date': '2019-11-11',
 	'campaign-end-date': '2019-12-31',
 	'banner-close-track-ratio': 1,
-	'goalDonationSum': 8700000
+	'goalDonationSum': 8700000,
+	'numberOfMembers': 73832
 };
