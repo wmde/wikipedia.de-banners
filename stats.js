ee759207e948ee191d1a647b55d3f5ed4af38a70
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-10-27 16:29:44 +0100

    Add C20_WPDE_mobile_01 banner
    - first mobile preact banner for wikipedia.de
    
    https://phabricator.wikimedia.org/T265253

--- stats.js
+++ stats.js
@@ -1,12 +1,12 @@
 GlobalBannerSettings = {
-	'donations-date-base': '2019-12-20',
+	'donations-date-base': '2020-11-03',
 	'donations-collected-base': 8001705,
 	'donators-base': 403966,
 	'appr-donations-per-minute': 97,
 	'appr-donators-per-minute': 3.8,
 	'impressions-per-day-in-million': 6,
-	'campaign-start-date': '2019-11-11',
-	'campaign-end-date': '2019-12-31',
+	'campaign-start-date': '2020-11-03',
+	'campaign-end-date': '2020-12-31',
 	'banner-close-track-ratio': 1,
 	'goalDonationSum': 8700000,
 	'numberOfMembers': 73832
