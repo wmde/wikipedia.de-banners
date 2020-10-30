6dfdf492e7d285cb207cde74c71a6ca1d556200e
KaiNissen <kai.nissen@wikimedia.de>
2020-10-30 16:41:37 +0100

    initialize values for the 2020 campaign

--- stats.js
+++ stats.js
@@ -1,13 +1,13 @@
 GlobalBannerSettings = {
 	'donations-date-base': '2020-11-03',
-	'donations-collected-base': 8001705,
-	'donators-base': 403966,
+	'donations-collected-base': 0,
+	'donators-base': 0,
 	'appr-donations-per-minute': 97,
 	'appr-donators-per-minute': 3.8,
 	'impressions-per-day-in-million': 6,
 	'campaign-start-date': '2020-11-03',
 	'campaign-end-date': '2020-12-31',
 	'banner-close-track-ratio': 1,
-	'goalDonationSum': 8700000,
+	'goalDonationSum': 9200000,
 	'numberOfMembers': 73832
 };
