ee18f46f9e524bb4d281d34bae8d56fdaea2a3a0
KaiNissen <kai.nissen@wikimedia.de>
2020-11-04 08:29:29 +0100

    postponed campaign start by one day

--- stats.js
+++ stats.js
@@ -1,11 +1,11 @@
 GlobalBannerSettings = {
-	'donations-date-base': '2020-11-03',
+	'donations-date-base': '2020-11-04',
 	'donations-collected-base': 100000,
 	'donators-base': 0,
 	'appr-donations-per-minute': 0,
 	'appr-donators-per-minute': 3.8,
 	'impressions-per-day-in-million': 6,
-	'campaign-start-date': '2020-11-03',
+	'campaign-start-date': '2020-11-04',
 	'campaign-end-date': '2020-12-31',
 	'banner-close-track-ratio': 1,
 	'goalDonationSum': 8700000,
