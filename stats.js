9cae8cd078bd0d172fd2e00340c6faa19e091302
KaiNissen <kai.nissen@wikimedia.de>
2021-11-03 12:54:07 +0100

    Change values for campaign start

--- stats.js
+++ stats.js
@@ -1,11 +1,11 @@
 GlobalBannerSettings = {
-	'donations-date-base': '2021-10-28',
+	'donations-date-base': '2021-11-03',
 	'donations-collected-base': 100000,
 	'donators-base': 1000,
 	'appr-donations-per-minute': 100,
 	'appr-donators-per-minute': 5,
 	'impressions-per-day-in-million': 8,
-	'campaign-start-date': '2021-10-28',
+	'campaign-start-date': '2021-11-03',
 	'campaign-end-date': '2021-12-31',
 	'banner-close-track-ratio': 1,
 	'goalDonationSum': 9200000,
