e4cbfdce3215c9e67a020f31373eda8239dd0ad1
KaiNissen <kai.nissen@wikimedia.de>
2021-11-19 14:45:29 +0100

    update daily banner impressions

--- stats.js
+++ stats.js
@@ -4,7 +4,7 @@ GlobalBannerSettings = {
 	'donators-base': 75200,
 	'appr-donations-per-minute': 100,
 	'appr-donators-per-minute': 4,
-	'impressions-per-day-in-million': 7,
+	'impressions-per-day-in-million': 6,
 	'campaign-start-date': '2021-11-03',
 	'campaign-end-date': '2021-12-31',
 	'banner-close-track-ratio': 1,
