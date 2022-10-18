2ab53896263879cf19b9f3e42e97b5ac7c3622ab
Abban Dunne <abban.dunne@wikimedia.de>
2022-10-18 06:54:23 +0200

    Add desktop test 01 banners

--- stats.js
+++ stats.js
@@ -1,12 +1,12 @@
 GlobalBannerSettings = {
-	'donations-date-base': '2021-11-12',
+	'donations-date-base': '2022-11-12',
 	'donations-collected-base': 2200000,
 	'donators-base': 345493,
 	'appr-donations-per-minute': 100,
 	'appr-donators-per-minute': 4,
 	'impressions-per-day-in-million': 6,
-	'campaign-start-date': '2021-11-03',
-	'campaign-end-date': '2021-12-31',
+	'campaign-start-date': '2022-11-03',
+	'campaign-end-date': '2022-12-31',
 	'banner-close-track-ratio': 1,
 	'goalDonationSum': 9200000,
 	'numberOfMembers': 73832
