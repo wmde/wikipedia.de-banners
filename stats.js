eab690ba663bda4c2c8baf036347ddd2cca6e304
KaiNissen <kai.nissen@wikimedia.de>
2022-11-04 11:44:56 +0100

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,10 +1,10 @@
 GlobalBannerSettings = {
 	'donations-date-base': '2022-11-03',
 	'donations-collected-base': 450000,
-	'donators-base': 30600,
+	'donators-base': 5400,
 	'appr-donations-per-minute': 100,
-	'appr-donators-per-minute': 4,
-	'impressions-per-day-in-million': 0,
+	'appr-donators-per-minute': 9,
+	'impressions-per-day-in-million': 7,
 	'campaign-start-date': '2022-11-03',
 	'campaign-end-date': '2022-12-31',
 	'banner-close-track-ratio': 1,
