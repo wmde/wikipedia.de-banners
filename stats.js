66c6cfff897b0344d216f3f773b0eee4aafeb30c
Gabriel Birke <gabriel.birke@wikimedia.de>
2024-10-08 17:17:16 +0200

    Update stats.js for 2024 campaign
    - The start date is beginning of october to test the progress bar
    - The donation target is the one form last year

--- stats.js
+++ stats.js
@@ -1,7 +1,7 @@
 campaignParameters = {
 	campaignProjection: {
 		donationTarget: 9700000,
-    	updatedAt: '2023-12-14',
+    	updatedAt: '2024-10-08',
 		donationSumBase: 7183034,
 		donationCountBase: 271072,
 		donationAmountPerMinute: 104.314,
@@ -9,27 +9,13 @@ campaignParameters = {
 		averageAmountPerDonation: 22.25
 	},
     millionImpressionsPerDay: 5,
-    startDate: '2023-10-30',
-    endDate: '2023-12-31',
+    startDate: '2024-10-01',
+    endDate: '2024-12-31',
     numberOfMembers: 73832,
-	isLateProgress: true,
+	isLateProgress: false,
 	thankYouCampaign: {
 		numberOfDonors: 350000,
 		progressBarPercentage: 100
 	}
 };
 
-// Legacy values to display pre-2023 banners, delete at some point
-GlobalBannerSettings = {
-	'donations-date-base': campaignParameters.campaignProjection.updatedAt,
-	'donations-collected-base': campaignParameters.campaignProjection.donationSumBase,
-	'donators-base': campaignParameters.campaignProjection.donationCountBase,
-	'appr-donations-per-minute': campaignParameters.campaignProjection.donationAmountPerMinute,
-	'appr-donators-per-minute': campaignParameters.campaignProjection.donationCountPerMinute,
-	'impressions-per-day-in-million': campaignParameters.millionImpressionsPerDay,
-	'campaign-start-date': campaignParameters.startDate,
-	'campaign-end-date': campaignParameters.endDate,
-	'banner-close-track-ratio': 1,
-	'goalDonationSum': campaignParameters.campaignProjection.donationTarget,
-	'numberOfMembers': campaignParameters.numberOfMembers
-};
