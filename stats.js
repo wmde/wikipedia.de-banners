6b046845a78c41958e62e2303720fa7d74218aa9
Kai Nissen <kai.nissen@wikimedia.de>
2024-10-24 15:34:46 +0200

    prepare for campaign start

--- stats.js
+++ stats.js
@@ -1,17 +1,17 @@
 campaignParameters = {
 	campaignProjection: {
-		donationTarget: 9700000,
-    	updatedAt: '2024-10-08',
-		donationSumBase: 7183034,
-		donationCountBase: 271072,
+	donationTarget: 9300000,
+	updatedAt: '2024-10-24',
+		donationSumBase: 100000,
+		donationCountBase: 1000,
 		donationAmountPerMinute: 104.314,
 		donationCountPerMinute: 3.450,
 		averageAmountPerDonation: 22.25
 	},
-    millionImpressionsPerDay: 5,
-    startDate: '2024-10-01',
-    endDate: '2024-12-31',
-    numberOfMembers: 73832,
+	millionImpressionsPerDay: 5,
+	startDate: '2024-10-28',
+	endDate: '2024-12-31',
+	numberOfMembers: 73832,
 	isLateProgress: false,
 	thankYouCampaign: {
 		numberOfDonors: 350000,
