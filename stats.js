bd19ba825d24450ccce6bb1a93abc5c67483e225
Kai Nissen <kai.nissen@wikimedia.de>
2025-11-08 12:30:36 +0100

    update projection factors

--- stats.js
+++ stats.js
@@ -4,8 +4,8 @@ campaignParameters = {
 		updatedAt: '2025-10-27',
 		donationSumBase: 273_000,
 		donationCountBase: 11500,
-		donationAmountPerMinute: 156.490,
-		donationCountPerMinute: 5.305,
+		donationAmountPerMinute: 144.331,
+		donationCountPerMinute: 4.939,
 		averageAmountPerDonation: 22.89
 	},
 	millionImpressionsPerDay: 5,
